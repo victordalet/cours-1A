33f5e8f3e7698924704925c7e465a9527e8b375a
unknown <victordalet@protonmail.com>
2022-11-03 17:18:34 +0100

    03 11

--- web/TP 1/script.js
+++ web/TP 1/script.js
@@ -8,6 +8,7 @@ const title = document.querySelector(".fixed-title");
 btn.addEventListener("click", () => {
     spinContainer.classList.toggle("active");
     if (screen.width < 1000) {
+        console.log(2);
         button.classList.toggle("active");
         title.classList.toggle("active");
     }
@@ -27,7 +28,7 @@ console.log(slidingNewsLetter);
 const multiplicator = 0.7;
 
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('wheel',()=>{
   const {scrollTop,clientHeight} = document.documentElement;
   let topElementToTopViewport = [];
 
@@ -38,6 +39,7 @@ window.addEventListener('scroll',()=>{
   for (let nb_slide = 0 ; nb_slide < nb_slide_max ; nb_slide++) {
     if(scrollTop > (scrollTop+topElementToTopViewport[nb_slide]).toFixed()-clientHeight*multiplicator){
       slidingNewsLetter[nb_slide].classList.add('active'+(nb_slide+1).toString());
+
     } 
   }
 
@@ -57,6 +59,9 @@ btn_light.addEventListener("click",()=> {
     root.style.setProperty('--colorDark','#fff');
     root.style.setProperty('--color3','#DE9400');
     scene.background = new THREE.Color(0x000000);
+    for (let i = 0 ; i < max_cube ; i++){
+      liste[i].material.color.setHex( 0xDE9400 );
+    }
     mod = false;
   }
   else {
@@ -64,6 +69,9 @@ btn_light.addEventListener("click",()=> {
     root.style.setProperty('--colorDark','#000');
     root.style.setProperty('--color3','#F9F99A');
     scene.background = new THREE.Color(0xffffff);
+    for (let i = 0 ; i < max_cube ; i++){
+      liste[i].material.color.setHex( 0xF9F99A );
+    }
     mod = true;
   }
 });
@@ -87,8 +95,11 @@ document.onmousemove = function() {
 /***************THREE******************/
 /**************************************/
 
-
 let camera, scene, renderer, cube;
+let liste = [];
+const max_cube = 30;
+let type1_scroll = 1;
+const speed_deplacement = 0.02;
 
 function init() {
   scene = new THREE.Scene();
@@ -97,27 +108,59 @@ function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-  const geometry = new THREE.BoxGeometry(4,2.5,3);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  cube.position.x = -13;
+  for (let i = 0 ; i<max_cube ; i++){
+    const geometry = new THREE.SphereGeometry(0.4);
+    const material = new THREE.MeshBasicMaterial({ color: 0xF9F99A });
+    cube = new THREE.Mesh(geometry, material);
+    cube.position.x = i-8;
+    if (i%2) {
+      cube.position.y = 1;
+    }
+    else{
+      cube.position.y = -2;
+    }
+    liste.push(cube);
+  }
+  for (let i = 0; i < max_cube ; i++){
+    scene.add(liste[i]);
+  }
   camera.position.z = 5;
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  window.addEventListener('scroll',rotate); // wheel 
+  window.addEventListener('scroll',rotate);
+  window.addEventListener('wheel',rotate); 
   renderer.render(scene, camera);
 }
 
 function rotate() {
-  //cube.rotation.x += 0.05;
-  cube.rotation.y += 0.05;
-  cube.position.x += 0.15;
-  console.log(cube.position.x);
-  if (cube.position.x > 12){
-    cube.position.x = -12;
+  for (let i = 0 ; i<max_cube ; i++){
+    liste[i].rotation.y += 0.002;
+    liste[i].rotation.z += 0.05;
+    liste[i].rotation.x += 0.1;
+    if (i%2){
+      if (type1_scroll == 1){
+        liste[i].position.y -= speed_deplacement;
+      }
+      else {
+        liste[i].position.y += speed_deplacement;
+      }
+      if (liste[i].position.y <= -2){
+        type1_scroll = 0;
+      } 
+      if (liste[i].position.y >= 1 ){
+        type1_scroll = 1;
+      }
+    }
+    else {
+      if (type1_scroll == 1){
+        liste[i].position.y += speed_deplacement;
+      }
+      else {
+        liste[i].position.y -= speed_deplacement;
+      }
+    }
   }
 }
 
---
+++ web/TP1 PHP/index.php
@@ -0,0 +1,94 @@
+<?php
+	/* *********************************************************************** */
+	/*                                                                         */
+	/* OBJECTIVE : TP 1 PHP                    #####      ###    ###    #      */
+	/* AUTHOR :  VICTOR DALET                  #         #      #       #      */
+	/* CREATED : 03 11 2022                    ####      #      #  ##   #      */
+	/* UPDATE  : 03 11 2022                    #         #      #   #   #      */
+	/*                                         ####    ###      #####   #.fr   */
+	/* *********************************************************************** */
+
+	$message  = "Salut";
+	$age = 17;
+	$var = ($age>1) ? "s." : ".";
+	$table = ["Banane", "Fraise", "Orange", "Kiwi"];
+	//var_dump($table);
+	echo "<h1>".$message."</h1>";
+	echo "<h2>Tu as ".$age."an".$var."</h2>";
+	if ($age<18){
+		echo "<h2>Tu es mineur.</h2>";
+	}
+	else {
+		echo "<h2>Tu es majeur.</h2>";
+	}
+	echo "<ul>";
+	foreach ($table as $key => $value) {
+		echo "<li>".$value."</li>";
+	}
+	echo "</ul>";
+	//table
+	echo"	<table>
+				<thead>
+				<tr>
+					<th>NOM</th>
+					<th>COULEUR</th>
+				</tr>
+			</thead>
+			<tbody>";
+	foreach ($table as $key => $value){
+		switch ($value) {
+			case 'Banane':
+				$couleur = "yellow";
+				break;
+			case 'Fraise':
+				$couleur = 'red';
+				break;
+			case 'Orange':
+				$couleur = 'orange';
+				break;
+			case 'Kiwi':
+				$couleur = "green";
+				break;
+			
+		}
+		echo "	<tr>
+					<td>".$value."</td>
+					<td style='color:".$couleur."'>".$couleur."</td>
+				</tr>
+					";
+	}
+	echo"
+			</tbody>
+			</table>";
+
+	$user1  = ["name"=>"dalet","firstname"=>"victor","email"=>"victordalet@protonmail.com"];
+	$user2  = ["name"=>"dalet","firstname"=>"victor","email"=>"victordalet@protonmail.com"];
+	$user3  = ["name"=>"dalet","firstname"=>"victor","email"=>"victordalet@protonmail.com"];
+
+	echo"	<table>
+				<thead>
+				<tr>
+					<th>Name</th>
+					<th>Firstname</th>
+					<th>Email</th>
+				</tr>
+			</thead>
+			<tbody>";
+	echo "<tr>";
+	echo"<td>".$user1["name"]."</td>";
+	echo"<td>".$user1["firstname"]."</td>";
+	echo"<td><a href=mailto:".$user1["email"].">".$user1["email"]."</a></td>";
+	echo"</tr>";
+	echo "<tr>";
+	echo"<td>".$user2["name"]."</td>";
+	echo"<td>".$user2["firstname"]."</td>";
+	echo"<td><a href=mailto:".$user2["email"].">".$user2["email"]."</a></td>";
+	echo"</tr>";
+	echo "<tr>";
+	echo"<td>".$user3["name"]."</td>";
+	echo"<td>".$user3["firstname"]."</td>";
+	echo"<td><a href=mailto:".$user3["email"].">".$user3["email"]."</a></td>";
+	echo"</tr>";
+
+
+?>
